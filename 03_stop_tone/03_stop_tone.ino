e57e04340cd5e0c775792f5bde028cb913e31d09
Electroviron <electroviron@gmail.com>
2026-09-20 19:27:27 +0100

    final pieces

--- 03_stop_tone/03_stop_tone.ino
+++ 03_stop_tone/03_stop_tone.ino
@@ -1,7 +1,10 @@
 const int BUZZER_PIN = 8;
 
 void setup() {
-  tone(BUZZER_PIN, 400);
+  tone(BUZZER_PIN, 1000);
+  delay(2000);
+
+  noTone(BUZZER_PIN);
 }
 
 void loop() {
---
+++ 07_melody/07_melody.ino
@@ -0,0 +1,14 @@
+/*
+ * CHALLENGE :)
+ 
+ * Create your own melody.
+ 
+ * Try changing:
+ *   -> The notes
+ *   -> The order of the notes
+ *   -> The duration of each note
+ *
+ * BONUS:
+  Can you make the melody play repeatedly
+ * instead of only once?
+ */
